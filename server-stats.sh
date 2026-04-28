c3423c551c5c22209b77a1e37aa576008a6c9109
thxgo <paduthiago02@gmail.com>
2026-04-27 23:58:21 -0300

    style: translate output messages to english

--- server-stats.sh
+++ server-stats.sh
@@ -6,18 +6,17 @@ show_system_info() {
     local uptime=$(uptime -p | sed 's/up //')
     local users_logged_in=$(who)
     
-    echo "Informações do sistema:"
-    echo "Versão do Sistema Operacional: ${os_version}"
-    echo "Versão do Kernel: ${kernel_version}"
-    echo -e "\nTempo de atividade do sistema: $uptime"
-    echo -e "Usuários logados:\n${users_logged_in}"
+    echo "System Information:"
+    echo "OS Version: ${os_version}"
+    echo "Kernel Version: ${kernel_version}"
+    echo -e "\nSystem Uptime: $uptime"
+    echo -e "Logged in users:\n${users_logged_in}"
 }
 
 show_cpu_usage() {
     local cpu_stats=$(top -bn1 | grep 'Cpu(s)' | cut -d"," -f4 | awk '{print 100-$1 "%"}')
 
-    echo -e "\nStatus do CPU do sistema:"
-    echo "Uso do CPU: ${cpu_stats}"
+    echo -e "\nCPU Usage: ${cpu_stats}"
 }
 
 show_memory_usage() {
@@ -26,28 +25,28 @@ show_memory_usage() {
     local mem_usedperc=$(awk "BEGIN {printf \"%.2f%%\", $mem_used/$mem_total * 100}")
     local mem_freeperc=$(awk "BEGIN {printf \"%.2f%%\", 100 - $mem_used/$mem_total * 100}")
 
-    echo -e "\nStatus da memória do sistema:"
-    echo "Memoria total: ${mem_total} MB"
-    echo "Memoria usada: ${mem_used} MB (${mem_usedperc})"
-    echo "Memoria livre: ${mem_free} MB (${mem_freeperc})"
+    echo -e "\nMemory Usage:"
+    echo "Total Memory: ${mem_total} MB"
+    echo "Used Memory: ${mem_used} MB (${mem_usedperc})"
+    echo "Free Memory: ${mem_free} MB (${mem_freeperc})"
 }
 
-show_disk_usage() {
+show_disk_usage() { 
     local str_total str_used str_free str_usedperc
     read str_total str_used str_free str_usedperc <<< $(df -h / | awk 'NR==2 {print $2, $3, $4, $5}')
     local str_freeperc=$(awk "BEGIN {printf \"%.0f%%\", 100 - ${str_usedperc%\%}}")
 
-    echo -e "\nStatus do disco do sistema:"
-    echo "Armazenamento total: ${str_total}B"
-    echo "Armazenamento usado: ${str_used}B (${str_usedperc})"
-    echo -e "Armazenamento livre: ${str_free}B (${str_freeperc})"
+    echo -e "\nDisk Usage:"
+    echo "Total Storage: ${str_total}B"
+    echo "Used Storage: ${str_used}B (${str_usedperc})"
+    echo -e "Free Storage: ${str_free}B (${str_freeperc})"
 }
 
 show_top_processes() {
-    echo -e "\nProcessos com maior uso de RAM:"
+    echo -e "\nTop Processes by RAM Usage:"
     ps aux --sort -%mem | head -n 6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $11}'
 
-    echo -e "\nProcessos com maior uso de CPU:"
+    echo -e "\nTop Processes by CPU Usage:"
     ps aux --sort -%cpu | head -n 6 | awk '{print $1 "\t" $2 "\t" $3 "\t" $11}'
 }
 
